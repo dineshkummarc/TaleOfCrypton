39478e24b59209de108467cb6053562e9975495c
Dinesh Kumar <mapmydestination@gmail.com>
2024-09-14 23:45:43 +0530

    update

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'https://toc19.isteknit.org/';
+$config['base_url'] = 'http://local.rocket.co.in:8081/TaleOfCrypton/';
 
 /*
 |--------------------------------------------------------------------------
