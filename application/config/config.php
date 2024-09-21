6b0b018df82e727604e228cde48f7b6a945f092a
Dinesh Kumar <mapmydestination@gmail.com>
2024-09-21 08:50:12 +0530

    update

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://local.rocket.co.in:8081/TaleOfCrypton/';
+$config['base_url'] = 'http://localhost/TaleOfCrypton/';
 
 /*
 |--------------------------------------------------------------------------
